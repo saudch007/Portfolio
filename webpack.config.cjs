9adce4c846bc3643e8bef98bc93a15d88aed11f2
Saud <f191001@nu.edu.pk>
2023-08-03 19:50:24 +0500

    - Webpack included

---
+++ webpack.config.cjs
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./main.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
